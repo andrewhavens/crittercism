9654e1998c1e4698b8025d32c745690cd2673a22
Andrew Havens <email@andrewhavens.com>
2015-11-05 13:51:58 -0800

    Simplify code for handling dsym upload

--- crittercism.gemspec
+++ crittercism.gemspec
@@ -2,7 +2,7 @@
 
 Gem::Specification.new do |spec|
   spec.name          = "crittercism"
-  spec.version       = "0.1.0"
+  spec.version       = "0.1.1"
   spec.authors       = ["Andrew Havens"]
   spec.email         = ["email@andrewhavens.com"]
 
--- lib/crittercism/rake_tasks.rb
+++ lib/crittercism/rake_tasks.rb
@@ -24,30 +24,21 @@ namespace :crittercism do
 
   desc 'Submit .dSYM to Crittercism'
   task :upload_dsym => :zip_dsym do
-    require 'net/http'
-    require 'net/http/post/multipart'
-
     puts 'Submitting dSYM to Crittercism...'
 
     upload_uri = "https://api.crittercism.com/api_beta/dsym/#{App.config.crittercism_app_id}"
-    uri = URI.parse(upload_uri)
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
+    command = "/usr/bin/curl #{upload_uri}"\
+                 " --silent --output /dev/null -w '%{http_code}'"\
+                 " -F dsym=@'#{app_dsym}.zip'"\
+                 " -F key='#{App.config.crittercism_api_key}'"
 
-    request = Net::HTTP::Post::Multipart.new(uri.path,
-      'dsym' => UploadIO.new("#{app_dsym}.zip", 'application/zip', 'dsym.zip'),
-      'key' => App.config.crittercism_api_key
-    )
-    response = http.request(request)
-
-    puts "Crittercism Response: #{response.code} #{response.message}"
-    p response.body unless /^2\d+/.match("#{response.code}")
-
-    # TODO: use simple curl command once I can figure out how to display a helpful response
-    # curl = "/usr/bin/curl #{upload_uri} "\
-    #        '--silent --output /dev/null '\
-    #        "-F dsym=@'#{app_dsym}.zip' "\
-    #        "-F key='#{App.config.crittercism_api_key}' "
-    # sh curl
+    output = %x[#{command}]
+    if output =~ /^2\d+/
+      puts 'Successfully uploaded dSYM to Crittercism.'
+    else
+      puts 'Failed to upload dSYM to Crittercism.'
+      p output
+      exit 1
+    end
   end
 end
